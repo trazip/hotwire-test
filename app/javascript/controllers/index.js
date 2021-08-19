75a69b71258c1116ee538d4efb44c38b0f3c236f
Pierre Penhard <p.penhard@gmail.com>
2021-08-19 21:40:50 +0200

    add style for rooms, stimulus components

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,4 +1,4 @@
-// Load all the controllers within this directory and all subdirectories. 
+// Load all the controllers within this directory and all subdirectories.
 // Controller files must be named *_controller.js or *_controller.ts.
 
 import { Application } from "stimulus"
@@ -7,3 +7,13 @@ import { definitionsFromContext } from "stimulus/webpack-helpers"
 window.Stimulus = Application.start()
 const context = require.context("controllers", true, /_controller\.(js|ts)$/)
 Stimulus.load(definitionsFromContext(context))
+
+import { Alert, Autosave, Dropdown, Modal, Tabs, Popover, Toggle, Slideover } from "tailwindcss-stimulus-components"
+Stimulus.register('alert', Alert)
+Stimulus.register('autosave', Autosave)
+Stimulus.register('dropdown', Dropdown)
+Stimulus.register('modal', Modal)
+Stimulus.register('tabs', Tabs)
+Stimulus.register('popover', Popover)
+Stimulus.register('toggle', Toggle)
+Stimulus.register('slideover', Slideover)
